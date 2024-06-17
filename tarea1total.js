0dd821416b7e9e07f1fb3beaa313c6035c129afb
ldarkeas <leas2002fi@gmail.com>
2024-06-16 18:38:57 -0600

    intento subir

---
+++ tarea1total.js
@@ -0,0 +1,68 @@
+//ejercicio numero 1
+// const libros = [
+//   { titulo: "Cien años de soledad", 
+//     autor: "Gabriel García Márquez", 
+//     ano: 1967 },
+
+//   { titulo: "Don Quijote de la Mancha", 
+//     autor: "Miguel de Cervantes", 
+//     ano: 1605 },
+
+//   { titulo: "1984", 
+//     autor: "George Orwell", 
+//     ano: 1949 }
+// ];
+// const autores = [libros[0].autor,libros[1].autor,libros[2].autor]
+// console.log(autores)
+
+
+
+
+
+//ejercicio numero 2
+// const empleado = {nombre: 'Juan', salario: 10000, antiguedad: 6};
+
+// if (empleado.antiguedad>5){
+
+// empleado.salario = (empleado.salario*0.1)+empleado.salario
+// console.log(`el nombre del empleado es: ${empleado.nombre}`)
+// console.log(`el sueldo del empleado es ${empleado.salario}`)
+// console.log(`la antiguedad del empleado es: ${empleado.antiguedad}`)
+
+// console.log(empleado)
+// } else {
+
+//   console.log(`el nombre del empleado es: ${empleado.nombre}`)
+//   console.log(`el sueldo del empleado es ${empleado.salario}`)
+//   console.log(`la antiguedad del empleado es: ${empleado.antiguedad}`)
+//   console.log(empleado)
+// }
+
+
+///ejercicio numero 3 y 4
+// const numeros = [5, 8, 12, 3, 7, 19, 1, 10, 2]
+
+// const numeromayor = Math.max(...numeros);
+// console.log(`El numero mayor es:  ${numeromayor}`)
+ 
+// const numeroimpar = numeros.filter(numero => numero%2 !==0)
+// const sumaimpares = (numeroimpar.reduce((acumulador, numero)=> acumulador + numero,0))
+// const promedio = sumaimpares/(numeroimpar.length)
+// console.log(`el promedio de los numeros impares es: ${promedio}`)
+
+
+//ejercicio numero 5
+// const persona = {nombre: "Ana",
+//   edad: 25,
+//   ciudad: "SPS"
+// }
+
+// const name = persona.nombre
+// const age = persona.edad
+// const city = persona.ciudad
+
+// console.log(name)
+// console.log(age)
+// console.log(city)
+
+
